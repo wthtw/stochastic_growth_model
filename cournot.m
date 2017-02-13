da6f4d7931cfa6c3ca32abfdc4c94d89bc985114
Youssef de Madeen Amadou <madeenamadou@users.noreply.github.com>
2017-02-13 12:05:12 -0500

    Delete cournot.m

--- cournot.m
+++
@@ -1,6 +0,0 @@
-function [fval,fjac] = cournot(q)
-    c = [0.6; 0.8]; eta = 1.6; e = -1/eta;
-    fval = sum(q)^e + e*sum(q)^(e-1)*q - diag(c)*q;
-    fjac = e*sum(q)^(e-1)*ones(2,2) + e*sum(q)^(e-1)*eye(2) ...
-    + (e-1)*e*sum(q)^(e-2)*q*[1 1] - diag(c);
-end
