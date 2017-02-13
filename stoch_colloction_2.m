fa3ecf3a77dc57d8fe0e241efcea1ba3936d0d66
Youssef de Madeen Amadou <madeenamadou@users.noreply.github.com>
2017-02-13 12:18:31 -0500

    Update and rename sg_model_coll2.m to stoch_colloction_2.m

---
+++ stoch_colloction_2.m
@@ -0,0 +1,120 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   stoch_collocation.m:  A Matlab program to solve a simple stochastic growth 
+%                 model using Collocation Method.
+%
+%   Youssef de Madeen Amadou, Winter 2014
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initializing time chrono
+tic;
+clc;clear;
+
+%
+%  Parameter values
+%
+
+sigma = 1.50;       % utility parameter
+delta = 0.10;       % depreciation rate
+beta = 0.95;        % discount factor
+alpha = 0.30;       % capital elasticity of output
+rho = 0.80;         % persistence of the shock on Z
+epsi = 1e-5;        % convergence parameter
+
+% Discretization of the shocks, Markov AR(1) process
+nbe = 15;           % number of shocks, 15
+se = 0.12;          % volatility of the shock
+mu = 0;             % mean of shocks residuals iid normal process
+
+% Matrice of transition probabilities using tauchen and hussey
+muZ = 0.5;            % mean of shocks
+[Z,P] = tauchenhussey(nbe,muZ,rho,se,se);
+
+% Constructing grid values for K, using collocation method
+nbk     = 50;        % number of collocation points, 50
+kmin  = 0.2;
+kmax  = 6;
+basis = fundefn('lin',nbk,kmin,kmax);  % cubic spline basis   
+kgrid = funnode(basis);                    
+Phi   = funbas(basis);
+
+% Setting initial guess for basis coefficients c
+c = zeros(nbk,nbe);
+%load c;
+%load c_gscoll;
+
+% Initializating some matrices
+Tv = zeros(nbk,nbe); 
+dr = zeros(nbk,nbe);
+C = zeros(nbk,nbe);
+util = zeros(nbk,nbe);
+
+% Iterations
+for it = 1:4000
+    cold   = c;
+    for p = 1:nbk
+        temp1 = (kgrid(p).^alpha)*(Z') + (1-delta).*kgrid(p);
+        C = bsxfun(@plus,temp1,-kgrid);
+        util = bsxfun(@rdivide, C.^(1-sigma)-1, 1-sigma); % bsxfun do element-by-element operations between arrays, here a division
+        neg = C<0;
+        util(neg) = -1e12;
+        temp2 = bsxfun(@plus, util, beta*(funeval(c,basis,kgrid)*P));
+        [Tv(p,:), dr(p,:)] = max(temp2);
+    end
+
+c = Phi\Tv;
+crit = norm(c-cold);
+if crit<epsi, break,end
+
+end
+
+% Getting the optimal policy K'* for each K in the grid
+Kp = kgrid(dr);
+
+% Computing the optimal policy C* for each K in the grid
+temp2 = bsxfun(@times,(kgrid.^alpha),Z'); temp3 = bsxfun(@plus,temp2,(1-delta).*kgrid);
+C = bsxfun(@plus,temp3,-Kp);
+neg = C<0; C(neg) = 1e-6;
+util(neg) = -1e12;
+
+save ('Kp','Kp'); save ('C','C'); save ('t','t'); save ('c','c')
+
+
+%% Simulating the solved stochastic growth model
+
+T = 200;        % number of periods
+p = rand(T,1);  % uniformly distributed 'p' considered as probabilities
+Zsim = zeros(T+1,1);
+Zsim(1) = Z(9); % starting value for Z
+
+% Simulating the shocks, using probabilities in p
+for j=2:T+1
+        l = find(Z==Zsim(j-1));
+        m = find(p(j-1)<=cumsum(P(l,:),2),1,'first');
+        if find(p(j-1)>cumsum(P(l,:),2),1,'last') == m - 1;
+            Zsim(j)=Z(m);
+        end
+end
+
+Ksim = zeros(T+1,1);
+Ksim(1) = kgrid(kmax);
+Ysim = zeros(T,1);
+
+% Simulating Stock of capital and Production evolution
+% Knowing Zt-1, we interpolate the appropriate decision rule fonction for
+% Kt on the grid
+for t=1:T;
+Ysim(t) = Zsim(t)*(Ksim(t).^alpha);
+l = find(Z==Zsim(t));
+Ksim(t+1) = interp1(kgrid,Kp2(:,l),Ksim(t),'spline');  
+end
+
+% Computing implied Investment and Consumption
+Isim = Ksim(2:T+1)-(1-delta)*Ksim(1:T);
+Csim = Ysim-Isim;
+
+figure;
+subplot(2,2,1), plot(Csim,'r'), title('Consumption'), xlabel('Time');
+subplot(2,2,2), plot(Ysim,'b'), title('Production'), xlabel('Time');
+subplot(2,2,3), plot(Ksim,'r'), title('Stock of Capital'), xlabel('Time'), axis([1 T+1 0.2 6]);
+subplot(2,2,4), plot(Isim,'b'), title('Investment'), xlabel('Time');
